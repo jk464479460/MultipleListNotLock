6da37249cc964364f173f78bc122b120da29b7c5
jhon <464479460@qq.com>
2017-05-22 21:24:44 +0800

    add lock for normal fetch data

--- TestRand/Program.cs
+++ TestRand/Program.cs
@@ -5,13 +5,46 @@ using System.IO;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace TestRand
 {
     class Program
     {
+        static object obj = new object();
+        static int Get(List<string> list)
+        {
+            lock (obj)
+            {
+                if(list.Count>0)
+                {
+                    var item = list[list.Count - 1];
+                    list.RemoveAt(list.Count-1);
+                    Console.WriteLine("Do once");
+                }
+                return list.Count;
+            }
+        }
         static void Main(string[] args)
         {
+            var list = new List<string>();
+            for (var i = 0; i < 1000000; i++)
+                list.Add(Guid.NewGuid().ToString());
+            var start = new Stopwatch();
+            var startTime = DateTime.Now;
+            start.Start();
+            var task=Task.Factory.StartNew(()=> {
+                while (true)
+                    if (Get(list) == 0)
+                        break;
+            });
+            task.Wait();
+
+            start.Stop();
+
+            var time = start.ElapsedMilliseconds;
+            return;
+
             for(var i = 0; i<10; i++)
             {
                 var timeConsume = TimeConsume();
@@ -34,6 +67,7 @@ namespace TestRand
             start.Start();
             foreach (var i in list)
                 Console.WriteLine("Do once");
+
             start.Stop();
 
             return start.ElapsedMilliseconds;
